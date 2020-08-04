17396bfe955bca87520f2086f8ed149ee102331a
Sam Gielis <samgielis@gmail.com>
2020-08-04 17:51:47 +0200

    Add from field to query.

--- api/news.js
+++ api/news.js
@@ -4,7 +4,7 @@ const PAGE_ID = "215470341909937";
 
 export async function handler(event) {
   const PAGE_API_KEY = process.env.PAGE_API_KEY;
-  const url = `https://graph.facebook.com/v7.0/${PAGE_ID}/posts?fields=full_picture%2Cis_hidden%2Cis_published%2Cmessage%2Cpermalink_url&access_token=${PAGE_API_KEY}`;
+  const url = `https://graph.facebook.com/v7.0/${PAGE_ID}/posts?fields=full_picture%2Cis_hidden%2Cis_published%2Cmessage%2Cpermalink_url%2Cfrom&access_token=${PAGE_API_KEY}`;
 
   if (event.httpMethod !== "GET") {
     return {
