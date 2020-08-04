b8b16659c33a70572b4251ef720cc38f7bb9f6ce
Sam Gielis <samgielis@gmail.com>
2020-08-04 17:55:07 +0200

    Make it easer to add new fields to query.

--- api/news.js
+++ api/news.js
@@ -4,7 +4,16 @@ const PAGE_ID = "215470341909937";
 
 export async function handler(event) {
   const PAGE_API_KEY = process.env.PAGE_API_KEY;
-  const url = `https://graph.facebook.com/v7.0/${PAGE_ID}/posts?fields=full_picture%2Cis_hidden%2Cis_published%2Cmessage%2Cpermalink_url%2Cfrom&access_token=${PAGE_API_KEY}`;
+  let fields = [
+    "full_picture",
+    "is_hidden",
+    "is_published",
+    "message",
+    "permalink_url",
+    "from",
+  ];
+  fields = fields.join("%2C");
+  const url = `https://graph.facebook.com/v7.0/${PAGE_ID}/posts?fields=${fields}&access_token=${PAGE_API_KEY}`;
 
   if (event.httpMethod !== "GET") {
     return {
