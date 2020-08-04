e00394ac938e3fda9318fb4b5695d73836876dfa
Sam Gielis <samgielis@gmail.com>
2020-08-04 15:12:59 +0200

    Use JS instead of TS.

--- api/news.js
+++ api/news.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 const PAGE_ID = "215470341909937";
 
-export async function handler(
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> {
+export async function handler(event) {
   const PAGE_API_KEY = process.env.PAGE_API_KEY;
   const url = `https://graph.facebook.com/v7.0/${PAGE_ID}/posts?fields=full_picture%2Cis_hidden%2Cis_published%2Cmessage%2Cpermalink_url&access_token=${PAGE_API_KEY}`;
 
